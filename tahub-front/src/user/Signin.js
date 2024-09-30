8ad166f1b6e3e027868384253dca63089bab9fea
BharathKumarVelugu <149813928+bharat240899@users.noreply.github.com>
2024-09-30 11:22:30 -0400

    removed default creds

--- tahub-front/src/user/Signin.js
+++ tahub-front/src/user/Signin.js
@@ -7,8 +7,8 @@ import {signin,authenticate,isAuthenticated} from '../auth';
 const Signin =() =>{
     const [values,setValues] =useState({
       
-        email:'jon@mail.com',
-        password:'Bharath@24',
+        email:'',
+        password:'',
         error:'',
         loading:false,
         redirectToReferrer:false
